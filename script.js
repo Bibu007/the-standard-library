80d75f2ab6738439bb4a4b011bc8c0442abc46c8
Bibu007 <bibinbabu218@gmail.com>
2026-07-16 18:27:41 +0530

    Book and BookList classes created

--- script.js
+++ script.js
@@ -2,72 +2,78 @@ const add = document.querySelector(".add");
 const dialog = document.querySelector(".add-book-dialog");
 const closeBtn = document.querySelector("#close");
 const removeBtn = document.querySelector('#remove');
-const books = new Array();
 
-const myForm = document.getElementById("add-new");
-const formData = myForm.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent standard page reload
+class Book{
+    constructor(title, author, pages){
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = "Unread";
+    }
 
-  const formData = new FormData(myForm);
-  const formObject = Object.fromEntries(formData.entries());
-
-  console.log(formObject); 
-  // Output: { username: "JohnDoe", email: "john@example.com" }
-
-  addNewBookTolibrary(formObject.title, formObject.author, formObject.pages);
-
-  displayBooks();
-  
-
-  
-
-  dialog.close();
-  myForm.reset();
-});
-
-
-
-add.addEventListener('click', () => {dialog.showModal();});
-closeBtn.addEventListener('click', () => dialog.close());
-
-function Book(title, author, pages){
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = "Unread";
+    readBook(event){
+        if(event.target.textContent == "Read"){
+            event.target.textContent ="Unread";
+        }
+        else if(event.target.textContent == "Unread"){
+            event.target.textContent = "Read";
+        }
+    }
+    
 }
 
-const b1 = new Book("Titan", "Ron Chernov", "723");
-books.push(b1);
-const b2 = new Book("Rework", "Jason Fried", "356");
-books.push(b2);
-const b3 = new Book("Mossad", "Michal Bar Zohar", "567");
-books.push(b3);
-const b4 = new Book("Elon Musk", "Walter Issacson", "844");
-books.push(b4);
-const b5 = new Book("Nexus", "Yuval Noah Harari", "531");
-books.push(b5);
-const b6 = new Book("Sapiens", "Yuval Noah Harari", "724");
-books.push(b6);
-const b7 = new Book("Siddartha", "Herman Hesse", "221");
-books.push(b7);
-const b8 = new Book("Fountainhead", "Ayn Rand", "789");
-books.push(b8);
-const b9 = new Book("Metamorphosis", "Franz Kafka", "1853");
-books.push(b9);
-const b10 = new Book("Deep work", "Cal Newport", "312");
-books.push(b10);
+class BookList{
+    constructor(){
+        this.books = [];
+        this.bookMap = {};
+    }
 
-let bookMap = {};
+    addNewBookTolibrary(title, author, pages){
+    const b = new Book(title, author, pages);
+    this.books.push(b);
+    this.bookMap[b.id] = this.books.length - 1;
+    }
 
-for(let i = 0; i < books.length; i++){
-bookMap[books[i].id] = i;
+    displayBooks(){
+        tableBody.replaceChildren();
+        for(let i = 0; i < this.books.length; i++){
+            const rowTemplate = `<tr>
+                    <td>${this.books[i].title}</td>
+                    <td>${this.books[i].author}</td>
+                    <td>${this.books[i].pages}</td>
+                    <td><button class="table-btn read-btn" id="read">${this.books[i].read}</button></td>
+                    <td><button class="table-btn remove-btn" id="remove" data-id="${this.books[i].id}">Remove</button></td>
+                    </tr>`;
+            tableBody.insertAdjacentHTML("beforeend", rowTemplate);
+        }
+    }
+
+    removeBook(event){
+        let bookIndex = this.bookMap[event.target.dataset.id];
+        this.books.splice(bookIndex, 1);
+        this.displayBooks();
+    }
+
 }
 
+const l = new BookList();
+
+l.addNewBookTolibrary("Titan", "Ron Chernov", "723");
+l.addNewBookTolibrary("Rework", "Jason Fried", "356");
+l.addNewBookTolibrary("Mossad", "Michal Bar Zohar", "567");
+l.addNewBookTolibrary("Elon Musk", "Walter Issacson", "844");
+l.addNewBookTolibrary("Nexus", "Yuval Noah Harari", "531");
+l.addNewBookTolibrary("Sapiens", "Yuval Noah Harari", "724");
+l.addNewBookTolibrary("Siddartha", "Herman Hesse", "221");
+l.addNewBookTolibrary("Fountainhead", "Ayn Rand", "789");
+l.addNewBookTolibrary("Metamorphosis", "Franz Kafka", "153");
+l.addNewBookTolibrary("Deep work", "Cal Newport", "312");
+
+console.log(l.books);
 
 const tableBody = document.querySelector(".books-table");
-
+/*
 function displayBooks(){
     tableBody.replaceChildren();
     for(let i = 0; i < books.length; i++){
@@ -82,14 +88,18 @@ function displayBooks(){
         tableBody.insertAdjacentHTML("beforeend", rowTemplate);
     }
 }
+*/
 
-displayBooks();
+l.displayBooks();
 
+/*
 function addNewBookTolibrary(title, author, pages){
     const b = new Book(title, author, pages);
     books.push(b);
 }
+*/
 
+/*
 function removeBook(event){
     let bookIndex = bookMap[event.target.dataset.id];
     books.splice(bookIndex, 1);
@@ -104,13 +114,13 @@ function readBook(event){
         event.target.textContent = "Read";
     }
 }
-
+*/
 //removeBtn.addEventListener('click', (event) => removeBook(event));
 
 tableBody.addEventListener('click', (event) => {
   // Check if the clicked element is actually the remove button
   if (event.target.classList.contains('remove-btn')) {
-    removeBook(event);
+    l.removeBook(event);
   }
 
 });
@@ -122,3 +132,31 @@ tableBody.addEventListener('click', (event) => {
     readBook(event);
   }
 });
+
+const myForm = document.getElementById("add-new");
+const formData = myForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent standard page reload
+
+  const formData = new FormData(myForm);
+  const formObject = Object.fromEntries(formData.entries());
+
+  console.log(formObject); 
+  // Output: { username: "JohnDoe", email: "john@example.com" }
+
+  l.addNewBookTolibrary(formObject.title, formObject.author, formObject.pages);
+
+  l.displayBooks();
+  
+
+  
+
+  dialog.close();
+  myForm.reset();
+});
+
+
+
+add.addEventListener('click', () => {dialog.showModal();});
+closeBtn.addEventListener('click', () => dialog.close());
+
+
